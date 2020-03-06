9b86cdb608c0b2255ca4067fecf42b6917be6007
Manlio Perillo <manlio.perillo@gmail.com>
2020-03-06 20:30:13 +0100

    Improve the Parse documentation
    In the Parse documentation, replace "selected Command" with
    "invoked Command", and clarify that the flags are those defined in the
    main commands and not in the main command itself.
    
    Replace Command.Usage with Command.defaultUsage in a comment.

--- cmd.go
+++ cmd.go
@@ -142,15 +142,16 @@ func (c *Command) usage() {
 	c.defaultUsage()
 }
 
-// Parse parses command-line from argument list, which should not include
-// the main command name, and return the selected Command.
+// Parse parses command-line from argument list, which should not include the
+// main command name, and return the invoked Command.
 //
-// Parse must be called after all flags in main.Flag are defined and before
-// flags are accessed by the program.  The return value will be flag.ErrHelp if
-// -help or -h were set but not defined.
+// Parse must be called after all flags in main commands Flag are defined and
+// before flags are accessed by the program.  The return value will be
+// flag.ErrHelp if -help or -h were set but not defined.
 func Parse(main *Command, argv []string) (*Command, error) {
 	// Configure main.Flag so that errors and output are in our control, but
-	// restore the output when returning, since Command.Usage will require it.
+	// restore the output when returning, since Command.defaultUsage will
+	// require it.
 	defer configure(main)()
 	if err := main.Flag.Parse(argv); err != nil {
 		return main, err
