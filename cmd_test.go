perillo/cmd

commit 0874ee242e44c14fb6b58ebb02b2dadeeed123a6

Don't return args in Parse
Change the Parse function to not return the command args, since it is
not necessary.

Update the Run function to get the args from cmd.Flag.Args().

cmd.go
@@ -112,17 +112,17 @@ func (c *Command) Usage() {
 // called after all flags in cmdline are defined and before flags are accessed
 // by the program.  The return value will be ErrHelp if -help or -h were set
 // but not defined.
-func Parse(main *Command, argv []string) (*Command, []string, error) {
+func Parse(main *Command, argv []string) (*Command, error) {
 	// Configure main.Flag so that errors and output are in our control, but
 	// restore the output when returning, since Usage will require it.
 	defer configure(&main.Flag)()
 	if err := main.Flag.Parse(argv); err != nil {
-		return main, nil, err
+		return main, err
 	}
 
 	args := main.Flag.Args()
 	if len(args) < 1 {
-		return main, args, ErrNoCommand
+		return main, ErrNoCommand
 	}
 
 	// TODO(mperillo): Sub commands are not currently supported.
@@ -141,15 +141,14 @@ func Parse(main *Command, argv []string) (*Command, []string, error) {
 			args = args[1:]
 		} else {
 			if err := cmd.Flag.Parse(args[1:]); err != nil {
-				return cmd, nil, err
+				return cmd, err
 			}
-			args = cmd.Flag.Args()
 		}
 
-		return cmd, args, nil
+		return cmd, nil
 	}
 
-	return main, main.Flag.Args(), ErrUnknownCommand
+	return main, ErrUnknownCommand
 }
 
 // configure configures f so that error handling is set to continue on errors
@@ -170,7 +169,8 @@ func configure(f *flag.FlagSet) (restore func()) {
 // Run parses the command-line from os.Args[1:] and execute the appropriate
 // sub command of the Main command.
 func Run() {
-	cmd, args, err := Parse(Main, os.Args[1:])
+	cmd, err := Parse(Main, os.Args[1:])
+	args := cmd.Flag.Args()
 	switch {
 	case err == ErrUnknownCommand:
 		fmt.Fprintf(os.Stderr, "%s %s: unknown command\n", Main.Name, args[0])

commit 8689c2a9df06967575f98976374b4d10af1e85d0

Fix Parse to correctly support Command.CustomFlags
Commit 0874ee2 (Don't return args in Parse) broke support for the
Command.CustomFlags, when set to true.

Change the Parse function to prepend the "--" terminator to the argument
list passed to the sub-command, when CustomFlags is true.

Add the ParseCustomFlags test.

cmd.go
@@ -164,11 +164,13 @@ func Parse(main *Command, argv []string) (*Command, error) {
 		// Configure cmd.Flag as it was done with main.Flag.
 		defer configure(cmd)()
 		if cmd.CustomFlags {
-			args = args[1:]
-		} else {
-			if err := cmd.Flag.Parse(args[1:]); err != nil {
-				return cmd, err
-			}
+			// Prepend the "--" terminator to the argument list of the
+			// sub-command, so that Flag.Parse will treat flags as regular
+			// arguments.
+			args = append([]string{"", "--"}, args[1:]...)
+		}
+		if err := cmd.Flag.Parse(args[1:]); err != nil {
+			return cmd, err
 		}
 
 		return cmd, nil

cmd_test.go
@@ -5,6 +5,7 @@
 package cmd
 
 import (
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -106,6 +107,29 @@ func TestParseFlag(t *testing.T) {
 	}
 	if arg != "arg" {
 		t.Errorf("got argument %q, want %q", arg, "arg")
+	}
+}
+
+// TestParseCustomFlags tests the Parse function, when the command has the
+// CustomFlags field set to true..
+func TestParseCustomFlags(t *testing.T) {
+	tree := list{"test", "cmd"}
+	argv := list{"test", "cmd", "-flag", "arg"}
+
+	main := build(tree)
+	main.Commands[0].CustomFlags = true
+	flag := main.Commands[0].Flag.Bool("flag", false, "flag")
+
+	cmd, err := Parse(main, argv[1:])
+	args := cmd.Flag.Args()
+	if err != nil {
+		t.Errorf("unexpected error %v", err)
+	}
+	if *flag {
+		t.Errorf("flag set")
+	}
+	if !reflect.DeepEqual(args, argv[2:]) {
+		t.Errorf("got arguments %q, want %q", args, argv[2:])
 	}
 }
 